f7e7697a6b8a61a86c4f64bc5c436d1c48a645ec
Juan Monsalvo <jmonsa13@gmail.com>
2022-06-25 16:02:10 -0500

    Update Visualization of time series analysis for disaster - Deployment Ready.

--- app.py
+++ app.py
@@ -100,5 +100,5 @@ register_callbacks(app, df_disaster, gdf)
 # ----------------------------------------------------------------------------------------------------------------------
 # Running the main code
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run_server(debug=False, host='0.0.0.0', port=8050)
     #app.run_server(debug=True, port=8050)
